d2ea1f96e0250ee4c03bae48517d990218e93268
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2022-04-27 12:36:47 +1000

    update

--- js/getstarted_add_active.js
+++ js/getstarted_add_active.js
@@ -6,20 +6,26 @@ jQuery(document).ready(function( $ ) {
     // $(".fullscreen").removeClass("active");
     // $(".fullscreen").hide();
     $( ".getstarted" ).click(function(e){
-        e.preventDefault()
-        //
-        $(this).toggleClass( "active" );
-        $( ".fullscreen" ).toggleClass( "active" );
-        $( "body" ).toggleClass( "no_scroll" );
-        // $("#header").toggleClass("reverse");
-        //
-        if ($( this ).hasClass( "active") ) {
-          $( this ).html("Close");
-        } 
-        
-        else {
-          $( this ).html("Get Started");
-          $("#header").removeClass("reverse");
+      e.preventDefault()
+      //
+      $(this).toggleClass( "active" );
+      $( ".fullscreen" ).toggleClass( "active" );
+      $( "body" ).toggleClass( "no_scroll" );
+      // $("#header").toggleClass("reverse");
+      //
+      if ($( this ).hasClass( "active") ) {
+        $( this ).html("Close");
+        $("#header").removeClass("reverse");
+      } 
+      else if ($( "#header" ).hasClass( "reverse") ) {    
+        $( this ).html("Get Started");
+        $("#header").removeClass("reverse");
+      } 
+      else {
+        $( this ).html("Get Started");
+        if(!$("#header").hasClass("shrink")){
+          $("#header").addClass("reverse");
         }
-    });
+      }
+  });
 });
