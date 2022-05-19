66e5585525b672fe848f5b2f205614f06fe120e2
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2022-05-20 03:03:39 +1000

    self checkout sign up forms

--- js/add_class_onscroll.js
+++ js/add_class_onscroll.js
@@ -1,18 +1,54 @@
 // add_class_onscroll .js
 
 // $.noConflict();
+// jQuery(document).ready(function( $ ) {
+//   $("#header").addClass("reverse");
+//     $(window).scroll(function() {
+//       if ($(this).scrollTop() > 50){
+//           $("#header").addClass("onscroll");
+//           $("#header").removeClass("reverse");
+//           $("#header").addClass("shrink");
+
+//       }
+//       else{
+//           $("#header").removeClass("onscroll");
+//           $("#header").addClass("reverse");
+//           $("#header").removeClass("shrink");
+//       }
+      
+//     });
+//   });
+
+// add_class_onscroll .js
+
+// $.noConflict();
 jQuery(document).ready(function( $ ) {
-  $("#header").addClass("reverse");
+
+	var do_not_reverse = Array('configure','configure','configure');
+	var body_id = jQuery("body").attr("id");
+	console.log(body_id);
+
+	if(jQuery.inArray(body_id, do_not_reverse) == -1) {
+		// only do when top of page 
+		$("#header").addClass("reverse");		
+	} else {
+		console.log(body_id);
+	}
+
     $(window).scroll(function() {
-      if ($(this).scrollTop() > 50){
+      if ($(this).scrollTop() > 20){
           $("#header").addClass("onscroll");
-          $("#header").removeClass("reverse");
+		  if(jQuery.inArray(body_id, do_not_reverse) == -1) {
+			$("#header").removeClass("reverse");
+		  }
           $("#header").addClass("shrink");
 
       }
       else{
           $("#header").removeClass("onscroll");
-          $("#header").addClass("reverse");
+		  if(jQuery.inArray(body_id, do_not_reverse) == -1) {
+			$("#header").addClass("reverse");
+		  }
           $("#header").removeClass("shrink");
       }
       if ($(this).scrollTop() > 200){
