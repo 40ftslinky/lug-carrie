ddca4d16682dbd4e89f6c9b4729c86124d59e99d
Jeremy Nicholson <40289122+40ftslinky@users.noreply.github.com>
2022-06-09 23:24:02 +1000

    updates to content and styles

--- js/add_class_onscroll.js
+++ js/add_class_onscroll.js
@@ -24,7 +24,7 @@
 // $.noConflict();
 jQuery(document).ready(function( $ ) {
 
-	var do_not_reverse = Array('configure','signup','accessory','operational','operational-zone','about', 'self-checkout-signup', 'book-a-test-ride', 'book-a-consultation');
+	var do_not_reverse = Array('configure','signup','accessory','operational','operational-zone','about', 'self-checkout-signup', 'book-a-test-ride', 'book-a-consultation', 'manual-checkout');
 	var body_id = jQuery("body").attr("id");
 	console.log(body_id);
 
